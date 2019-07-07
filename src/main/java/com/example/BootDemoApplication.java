522c5913cd1dc1638b93d68729a8bea0ff07c28c
Sachin Rokade <srokade918@gmail.com>
2019-07-07 13:47:46 +0530

    Update BootDemoApplication.java

--- src/main/java/com/example/BootDemoApplication.java
+++ src/main/java/com/example/BootDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootDemoApplication extends SpringBootServletInitializer {
+public class BootDemoApplication extends SpringBootServletInitializer //{
  
    	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
